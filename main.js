78fb70b775c7ed98cd0ce34cd0328ffb1918c680
2heeesss <hohooodo@gmail.com>
2020-08-27 03:41:16 +0900

    Add SumRatio function to calculate user data

--- main.js
+++ main.js
@@ -4,3 +4,31 @@ const menu = document.querySelector('.navbar-menu');
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+function excelExport(event) {
+  var input = event.target;
+  var reader = new FileReader();
+
+  reader.onload = function () {
+    var fileData = reader.result;
+    var wb = XLSX.read(fileData, { type: 'binary' });
+    wb.SheetNames.forEach(function (sheetName) {
+      let userData = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+      console.log(userData);
+
+      console.log(SumRatio());
+
+      function SumRatio() {
+        let badHabidRatio = 0;
+        let count = 0;
+
+        for (let i = 0; i < userData.length; i++) {
+          badHabidRatio += userData[i].비율;
+          count++;
+        }
+        return badHabidRatio / count;
+      }
+    });
+  };
+  reader.readAsBinaryString(input.files[0]);
+}
